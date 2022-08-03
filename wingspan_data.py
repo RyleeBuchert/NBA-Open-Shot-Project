e67d038a77dad529764d018ef1a1c1143a062042
RyleeBuchert <ryleebuchert@gmail.com>
2022-08-02 23:11:41 -0500

    Added height to shot data

--- wingspan_data.py
+++ wingspan_data.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+
 def get_wingspan(player_id):
     # Scrape html from url
     url = f"http://nbasavant.com/player.php?player_id={player_id}"
@@ -16,12 +17,27 @@ def get_wingspan(player_id):
     except:
         return('NA')
 
-def add_wingspan(row):
-    return get_wingspan(row['CLOSEST_DEFENDER_PLAYER_ID'])
 
-shot_data = pd.read_csv('shot_data.csv')
-# shot_data['def_wingspan'] = get_wingspan(shot_data['CLOSEST_DEFENDER_PLAYER_ID'])
-# print(get_wingspan(shot_data.loc[0]['CLOSEST_DEFENDER_PLAYER_ID']))
+def get_height(player_id):
+    # Scrape html from url
+    url = f"https://www.nba.com/player/{player_id}"
+    soup = BeautifulSoup(requests.get(url).content, 'html5lib')
 
-shot_data['closest_def_wingspan'] = shot_data.apply(lambda row: get_wingspan(row['closest_defender_id']), axis=1)
-shot_data.to_csv('shot_data2.csv')
+    # Extract and return height from data
+    try:
+        player_data = soup.find_all("p", {"class": "PlayerSummary_playerInfoValue__mSfou"})
+        player_text = player_data[0].contents[0].split(' ')[0].split("\'")
+        height = (int(player_text[0])*12) + int(player_text[1].replace("\"", ""))
+        return(height)
+    except:
+        return('NA')
+
+
+if __name__ == "__main__":
+
+    shot_data = pd.read_csv('shot_data.csv')
+
+    shot_data['closest_def_height'] = shot_data.apply(lambda row: get_height(row['closest_defender_id']), axis=1)
+    shot_data['closest_def_wingspan'] = shot_data.apply(lambda row: get_wingspan(row['closest_defender_id']), axis=1)
+    
+    shot_data.to_csv('shot_data2.csv')
